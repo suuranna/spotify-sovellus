19e8fe3ae9ffb753919fbf950f7d10627fc15a47
Anna Suuronen <annasuuronen@icloud.com>
2025-02-08 20:31:28 +0200

    tokenin hakeminen spotify APIsta

--- backend/app.js
+++ backend/app.js
@@ -6,12 +6,20 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors())
+app.use(express.json())
+
+const redirectUri =  'http://localhost:5173/frontpage'
+
+let clientId = process.env.CLIENT_ID
+//let code = ''
 
 const generateRandomString = () => {
   const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
   const values = crypto.getRandomValues(new Uint8Array(64))
   return values.reduce((acc, x) => acc + possible[x % possible.length], "")
 }
+
+const codeVerifier  = generateRandomString()
 
 const hashTheCodeVerifierWithSha256 = async (codeVerifier) => {
   const encoder = new TextEncoder()
@@ -24,31 +32,59 @@ const hashTheCodeVerifierWithSha256 = async (codeVerifier) => {
     .replace(/\//g, '_');
 }
 
+app.post('/api/token', async (request, response) => {
+  //const code_verifier = localStorage.getItem('code_verifier')
+  const code = request.body.code
+  console.log(code)
+
+  const url = 'https://accounts.spotify.com/api/token'
+  console.log('PARAMS:', clientId, code, redirectUri, codeVerifier)
+
+
+  const payload = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+      client_id: clientId,
+      grant_type: 'authorization_code',
+      code: code,
+      redirect_uri: redirectUri,
+      code_verifier: codeVerifier,
+    }),
+  }
+
+  const body = await fetch(url, payload);
+  const token = await body.json();
+
+  //const body = response1.body
+
+  console.log('BÄCKISSÄ')
+  console.log(token)
+  //response.send(response1);
+})
+
 app.get('/api/login', async (request, response) => {
   console.log('TÄÄLLÄÄ',  process.env.CLIENT_ID)
-  const codeVerifier  = generateRandomString();
-  console.log(codeVerifier)
+  //codeVerifier  = generateRandomString();
+  console.log('YEEEt',codeVerifier)
   const hashed = await hashTheCodeVerifierWithSha256(codeVerifier)
-  console.log('2',hashed)
+  code = hashed
+  //console.log('2',hashed)
   //window.localStorage.setItem('code_verifier', codeVerifier)
   const scope = 'user-read-private user-read-email'
   const authorizationEndpoint = "https://accounts.spotify.com/authorize"
   const params =  {
     response_type: 'code',
-    client_id: process.env.CLIENT_ID,
+    client_id: clientId,
     scope: scope,
     code_challenge_method: 'S256',
     code_challenge: hashed,
-    redirect_uri: 'http://localhost:5173',
+    redirect_uri: 'http://localhost:5173/frontpage',
   }
   const authUrl = new URL(authorizationEndpoint)
   authUrl.search = new URLSearchParams(params).toString()
-  //window.location.href = authUrl.toString()
-  console.log(authUrl.toString())
-  //response.header('Access-Control-Allow-Origin', 'http://localhost:5173'); 
-  //response.redirect(302, authUrl.toString())
-  //const spotifyResponse = await axios.get(authUrl.toString())
-  //console.log(spotifyResponse)
   response.status(201).json(authUrl.toString())
 })
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,74 @@
 //require('dotenv').config()
 //import './App.css'
 import axios from 'axios'
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from 'react-router-dom'
 
-const App = () => {
+let code_verifier = ''
+
+
+const getToken = async () => {
+  const urlParams = new URLSearchParams(window.location.search)
+  const code = urlParams.get('code')
+  console.log('Frontissa')
+  console.log(code)
+  console.log(typeof(code))
+  
+  const url = new URL(window.location.href);
+  url.searchParams.delete("code");
+
+  const updatedUrl = url.search ? url.href : url.href.replace('?', '');
+  window.history.replaceState({}, document.title, updatedUrl);
+
+  console.log(code)
+  const jotain = await axios.post('/api/token', {code: code})
+  console.log(jotain)
+}
+
+const FrontPage = () => {
+  const url = new URL(window.location.href);
+  //console.log(url)
+
+  if (url.href !== 'http://localhost:5173/frontpage') {
+    console.log('ifissä')
+    getToken()
+  }
+
+  return (
+    <div>
+      <p>Olet kirjautunut sisään</p>
+    </div>
+  )
+}
+
+const BeforeLoginPage = () => {
   const goToSpotifyLogin = async () => {
     
     const response = await axios.get('/api/login')
-    //const aa = anwer.then((response) => response.data)
     const authUrl = response.data
-    //console.log('frontissa', anwer.data)
     window.location.href = authUrl.toString()
-
-    const urlParams = new URLSearchParams(window.location.search)
-    let code = urlParams.get('code')
-
-    console.log(code)
 
 
   }
+
   return (
     <div>
       <p>spotify sovellus</p>
       <button onClick={goToSpotifyLogin}>kirjaudu</button>
     </div>
+  )
+}
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<BeforeLoginPage />} />
+        <Route path='/frontpage' element={<FrontPage />} />
+      </Routes>
+    </Router>
   )
 }
 
